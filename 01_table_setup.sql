85b7bf556aaf26d2763ab7b507d4bc24d086e04d
Spades <owenembleton96@gmail.com>
2026-04-08 22:19:57 -0500

    Added a listing_id foreign key to orders

--- 01_table_setup.sql
+++ 01_table_setup.sql
@@ -26,7 +26,9 @@ CREATE TABLE IF NOT EXISTS listings (
 CREATE TABLE IF NOT EXISTS orders (
 	order_id INT PRIMARY KEY,
 	user_id INT NOT NULL,
-	FOREIGN KEY (user_id) REFERENCES users(user_id)
+    listing_id INT NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (listing_id) REFERENCES listings(listing_id)
 );
 
 CREATE TABLE IF NOT EXISTS reviews (
